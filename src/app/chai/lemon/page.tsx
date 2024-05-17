1171974b6646808405c9d8a015b88e95e96b6aa2
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-05-17 15:34:25 +0530

    Start

--- src/app/chai/lemon/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function LemonPage(){
-    return(
-        <h1 className=" text-center text-3xl mt-10">Lemon Tea</h1>
-    )
-}
--- src/app/chai/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ChaiPage(){
-    return (
-        <h1 className=" text-center text-3xl mt-8">Chai Page</h1>
-    )
-}
