5536734eb6ffdbe747fa1eca98c5ba8d42994ed5
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-05-24 18:50:26 +0530

    git streek

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,10 @@
+
+
+function HeroSection() {
+  return (
+    <div className=" h-auto w-full rounded-md flex-col items-center justify-center relative overflow-hidden  mx-auto py-10"
+    >HeroSection</div>
+  )
+}
+
+export default HeroSection
